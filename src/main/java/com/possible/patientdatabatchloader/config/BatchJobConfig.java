162bd93e438102e0a1e706abfecceceae3125bfb
olayode <aolayode@getcarbon.co>
2022-01-28 15:31:41 +0100

    resolve dependency

---
+++ src/main/java/com/possible/patientdatabatchloader/config/BatchJobConfig.java
@@ -0,0 +1,4 @@
+package com.possible.patientdatabatchloader.config;
+
+public class BatchJobConfig {
+}
--- src/main/java/com/possible/patientdatabatchloader/domain/package-info.java
+++ src/main/java/com/possible/patientdatabatchloader/domain/package-info.java
@@ -2,3 +2,35 @@
  * JPA domain objects.
  */
 package com.possible.patientdatabatchloader.domain;
+
+
+
+//        compile "com.fasterxml.jackson.datatype:jackson-datatype-json-org"
+//        compile "com.fasterxml.jackson.datatype:jackson-datatype-hppc"
+//        compile "com.fasterxml.jackson.datatype:jackson-datatype-jsr310"
+//        compile "com.fasterxml.jackson.datatype:jackson-datatype-hibernate5"
+//        compile "com.fasterxml.jackson.core:jackson-annotations"
+//        compile "com.fasterxml.jackson.core:jackson-databind"
+//        compile "com.fasterxml.jackson.module:jackson-module-afterburner"
+//        compile "org.hibernate:hibernate-core"
+//        compile "com.zaxxer:HikariCP"
+//        compile "javax.transaction:javax.transaction-api"
+//        compile "org.hibernate:hibernate-envers"
+//        compile "org.hibernate.validator:hibernate-validator"
+//        compile "org.liquibase:liquibase-core"
+//        compile "com.mattbertolini:liquibase-slf4j:${liquibase_slf4j_version}"
+//        compile "org.springframework.boot:spring-boot-starter-actuator"
+//        compile "org.springframework.boot:spring-boot-autoconfigure"
+//        compile "org.springframework.boot:spring-boot-loader-tools"
+//        compile "org.springframework:spring-context-support"
+//        compile ("org.springframework.boot:spring-boot-starter-web") {
+//        exclude module: 'spring-boot-starter-tomcat'
+//        }
+//        compile "org.springframework.boot:spring-boot-starter-undertow"
+
+
+//        }
+//
+//        testCompile "org.assertj:assertj-core"
+
+
